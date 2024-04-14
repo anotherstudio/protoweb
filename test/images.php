386489bb78868fc8eca0ebc40c64f5099b8910d6
martinmana808 <martinmana808@gmail.com>
2024-04-15 09:54:53 +1200

    Fix typo in file name

--- test/images.php
+++ test/images.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 $folder = 'images/'; // Change this to the path of your image folder
 $images = array_diff(scandir($folder), array('.', '..')); // Remove '.' and '..' from the list
 
